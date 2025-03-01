83f86333f327bddc05ec61a7148c53a9fe8fef09
ayan878 <ayanraza197@gmail.com>
2025-03-01 12:18:04 +0530

    header & hero

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const floatingSection = document.getElementById("floatingSection");
 
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
-  let newSize = Math.min(20 + scrollY * .8, 200); 
+  let newSize = Math.min(20 + scrollY * .6, 200); 
 
   floatingSection.style.width = `${newSize}%`;
   floatingSection.style.height = `${newSize}vh`;
